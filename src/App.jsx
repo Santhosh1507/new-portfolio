71721476e5d3195ed01133e3229fa7ff86509e83
webdesign1507 <webdesign1507@gmail.com>
2024-06-15 13:53:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const [result, setResult] = React.useState("");
 
   const onPageScroll = () => {
-    if(window.pageYOffset > 200) {
+    if (window.pageYOffset > 200) {
       setScrolling(true)
     } else {
       setScrolling(false);
@@ -24,7 +24,7 @@ function App() {
 
   useEffect(() => {
     window.addEventListener("scroll", onPageScroll)
-    return() => {
+    return () => {
       window.removeEventListener("scroll", onPageScroll)
     }
   }, [])
@@ -125,16 +125,15 @@ function App() {
                   Furniture store landing page
                 </h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for online furniture store. HTML5,
-                  CSS3 (SCSS)
+                  Responsive HTML/CSS/Javascript layout for online furniture store.
                 </p>
                 <div className="flex mt-12 gap-2">
-                  <a className="flex-1 text-sm py-3 text-center bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <a href="https://furniture-store-landing-page.netlify.app/" className="flex-1 text-sm py-3 text-center bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                     Live preview
                   </a>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <a href="https://github.com/Santhosh1507/furniture-store/" className="flex-1 text-sm text-center py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" >
                     Checkout github
-                  </button>
+                  </a>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2">
@@ -147,12 +146,12 @@ function App() {
                   furniture. HTML5, CSS3 (SCSS)
                 </p>
                 <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <a href="" className="flex-1 text-sm py-3 text-center bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                     Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  </a>
+                  <a href="" className="flex-1 text-sm text-center py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" >
                     Checkout github
-                  </button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -167,12 +166,12 @@ function App() {
                   developer. HTML5, CSS3 (SCSS)
                 </p>
                 <div className="flex gap-4 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <a href="" className="flex-1 text-sm py-3 text-center bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                     Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  </a>
+                  <a href="" className="flex-1 text-sm text-center py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" >
                     Checkout github
-                  </button>
+                  </a>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2">
@@ -185,12 +184,12 @@ function App() {
                   (SCSS)
                 </p>
                 <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <a href="" className="flex-1 text-sm py-3 text-center bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                     Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  </a>
+                  <a href="" className="flex-1 text-sm text-center py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" >
                     Checkout github
-                  </button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -250,17 +249,17 @@ function App() {
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Teamwork
                 </p>
-              </div> 
+              </div>
               <div className="">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Problem-solving
                 </p>
-              </div> 
+              </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Communication 
+                  Communication
                 </p>
-              </div> 
+              </div>
             </div>
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-36 mt-4 sm:mt-6 w-[80%] max-w-4xl m-auto">
               <div>
@@ -310,47 +309,47 @@ function App() {
           <div className="container m-auto px-4">
             <h2 className="text-2xl font-semibold">Contant me</h2>
             <div className=" p-8 shadow-lg dark:bg-dark-2 sm:p-12 max-w-4xl m-auto">
-                <form className="flex flex-col gap-5" onSubmit={onSubmit}>
-                  <input
-                    type="text"
-                    name="name" 
-                    required
-                    placeholder="Your Name"
-                    className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  />
-                  <input
-                    type="text"
-                    name="email"
-                    required
-                    placeholder="Your Email"
-                    className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  />
-                  <input
-                    type="text"
-                    name="phone"
-                    required
-                    placeholder="Your Phone"
-                    className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  />
-                  <textarea
-                    typeof="text"
-                    placeholder="Your Message"
-                    name="message"
-                    required
-                    defaultValue=""
-                    className=" p-3 h-52 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  />
-                  <div>
-                    <button
-                      type="submit"
-                      className="w-full rounded-xl border-none bg-primary p-3 text-white transition hover:bg-blue-700 bg-cyan-500 hover:scale-105 duration-200"
-                    >
-                      Send Message
-                    </button>
-                  </div>
-                  <span className="spanresult text-center">{result}</span>
-                </form>
+              <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+                <input
+                  type="text"
+                  name="name"
+                  required
+                  placeholder="Your Name"
+                  className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
+                />
+                <input
+                  type="text"
+                  name="email"
+                  required
+                  placeholder="Your Email"
+                  className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
+                />
+                <input
+                  type="text"
+                  name="phone"
+                  required
+                  placeholder="Your Phone"
+                  className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
+                />
+                <textarea
+                  typeof="text"
+                  placeholder="Your Message"
+                  name="message"
+                  required
+                  defaultValue=""
+                  className=" p-3 h-52 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
+                />
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full rounded-xl border-none bg-primary p-3 text-white transition hover:bg-blue-700 bg-cyan-500 hover:scale-105 duration-200"
+                  >
+                    Send Message
+                  </button>
                 </div>
+                <span className="spanresult text-center">{result}</span>
+              </form>
+            </div>
           </div>
         </section>
       </main>
@@ -378,7 +377,7 @@ function App() {
       {
         scrolling && (
           <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
-            window.scrollTo(0,0);
+            window.scrollTo(0, 0);
           }}>
             <img src={ArrowDown} />
           </button>

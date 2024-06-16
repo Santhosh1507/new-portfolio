ed3f33b22039a0d8aa627ae836c0864d4fc59b6e
webdesign1507 <webdesign1507@gmail.com>
2024-06-16 17:55:29 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Project3 from "./assets/project3.png";
 import Project4 from "./assets/project4.png";
 import Facebook from "./assets/facebook.svg";
 import LinkedIn from "./assets/linkedin.svg";
-import Instagram from "./assets/instagram.svg";
 import ArrowDown from "./assets/arrow-down.svg";
 import { useEffect, useState } from "react";
 import React from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function App() {
   const [scrolling, setScrolling] = useState(false);
@@ -24,6 +26,13 @@ function App() {
   }
 
   useEffect(() => {
+    AOS.init({
+      offset: 80,
+      duration: 400,
+      easing: "ease-in-sine",
+      delay: 150,
+    });
+    AOS.refresh();
     window.addEventListener("scroll", onPageScroll)
     return () => {
       window.removeEventListener("scroll", onPageScroll)
@@ -55,9 +64,9 @@ function App() {
 
   return (
     <div className="max-w-7xl m-auto relative">
-      <header className={`${scrolling ? 'border-b border-gray-900' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
-        <div className="container m-auto px-4 py-6 max-w-7xl bg-black">
-          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+      <header className={`${scrolling ? 'border-b-2 border-gray-800' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
+        <div className=" m-auto px-4 py-6 max-w-7xl bg-black">
+          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="800">
             <div>
               <h1 className="font-bold text-2xl">Portfolio</h1>
             </div>
@@ -91,14 +100,14 @@ function App() {
       <main className="relative mt-28">
         {/* Intro/Banner section */}
         <section>
-          <div className="container m-auto px-4 pt-12 pb-12 sm:pt-20 flex flex-col-reverse sm:flex-row gap-6 text-center sm:text-left">
-            <div>
-              <h2 className="font-bold text-2xl sm:text-4xl">Hello, I'm Santhosh Kumar,</h2>
+          <div className="container m-auto px-4 pt-12 pb-12 sm:pt-20 flex flex-col-reverse sm:flex-row gap-6 text-center sm:text-left" data-aos="fade-right" data-aos-delay="100" data-aos-duration="1200">
+            <div className=" mt-16">
+              <h2 className="font-bold text-2xl sm:text-4xl" >Hello, I'm Santhosh Kumar,</h2>
               <div>
-                <h2 className="font-bold text-xl sm:text-4xl mt-1 gradiant-text">web developer</h2>
+                <h2 className="font-bold text-xl sm:text-4xl mt-1 gradiant-text" >web developer</h2>
               </div>
               <div>
-                <p className="mt-4 text-gray-400">
+                <p className="mt-4 text-gray-400" >
                   Former Full Stack Developer specialist, seeking to apply
                   competent development skills with focus on collaboration,
                   communication, and passion
@@ -110,8 +119,8 @@ function App() {
                 </a>
               </div>
             </div>
-            <div className="relative">
-              <div className="">
+            <div className="relative" >
+              <div className="" data-aos="fade-left" data-aos-delay="100" data-aos-duration="1200">
                 <img src={Porfile} className="relative z-10 w-[280px] m-auto sm:w-[600px] rounded-full" />
               </div>
             </div>
@@ -120,9 +129,9 @@ function App() {
         {/* Projects section */}
         <section id="projects">
           <div className="container m-auto px-4 sm:py-12">
-            <h2 className="text-2xl font-semibold">Projects</h2>
-            <div className="flex flex-col sm:flex-row gap-14 mt-11 max-w-5xl m-auto">
-              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:-rotate-2">
+            <h2 className="text-2xl font-semibold" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1200">Projects</h2>
+            <div className="flex flex-col sm:flex-row gap-14 mt-11 max-w-5xl m-auto" >
+              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:-rotate-2" data-aos="fade-up" data-aos-duration="800">
                 <img src={Project1} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
                   Furniture store landing page
@@ -139,7 +148,7 @@ function App() {
                   </a>
                 </div>
               </div>
-              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2">
+              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2" data-aos="fade-up" data-aos-duration="1200">
                 <img src={Project2} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
                   Designer coffee shop store landing page
@@ -159,7 +168,7 @@ function App() {
               </div>
             </div>
             <div className="flex flex-col sm:flex-row  gap-14 mt-11 max-w-5xl m-auto">
-              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:-rotate-2">
+              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:-rotate-2" data-aos="fade-up" data-aos-duration="800">
                 <img src={Project3} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
                   Landing page gym website for front-end developer
@@ -177,7 +186,7 @@ function App() {
                   </a>
                 </div>
               </div>
-              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2">
+              <div className="border border-gray-500 rounded-md p-5 flex-1 hover:scale-110 duration-100 hover:rotate-2" data-aos="fade-up" data-aos-duration="1200">
                 <img src={Project4} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
                   Website design for The frontend/backend developer project
@@ -200,8 +209,8 @@ function App() {
         {/* Technoglies section */}
         <section className="py-10" id="technologies">
           <div className="container m-auto px-4">
-            <h2 className="text-2xl font-semibold">Technologies</h2>
-            <div className="mt-14 max-w-4xl m-auto">
+            <h2 className="text-2xl font-semibold" data-aos="fade-right">Technologies</h2>
+            <div className="mt-14 max-w-4xl m-auto" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1200">
               <div>
                 <div className="flex justify-between items-center">
                   <h2 className="font-semibold">HTML</h2>
@@ -238,38 +247,38 @@ function App() {
         {/* Additional skills section */}
         <section>
           <div className="container m-auto px-4 py-14">
-            <h2 className="text-2xl font-semibold">
+            <h2 className="text-2xl font-semibold" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1200">
               Additional technologies and skills
             </h2>
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-28 mt-12 w-[80%] max-w-4xl m-auto">
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="800">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Git
                 </p>
               </div>
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1000">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Teamwork
                 </p>
               </div>
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1200">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Quick learning
                 </p>
               </div>
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1400">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Communication
                 </p>
               </div>
             </div>
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-36 mt-4 sm:mt-6 w-[80%] max-w-4xl m-auto">
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1600">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Problem-solving
                 </p>
               </div>
-              <div>
+              <div data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1800">
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   time management
                 </p>
@@ -279,10 +288,10 @@ function App() {
         </section>
         <section className="py-8" id="aboutme">
           <div className="container m-auto px-4">
-            <h2 className="text-2xl font-semibold">About me</h2>
-            <div className="mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-white">
-              <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
-                <h3 className="absolute left-0 text-lg font-semibold">2024</h3>
+            <h2 className="text-2xl font-semibold" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1200">About me</h2>
+            <div className="mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-white  max-w-5xl m-auto">
+              <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px] " data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600">
+                <h3 className="absolute left-0 text-lg font-semibold" >2024</h3>
                 <p>
                   Graduated from Meenakshi College with a B.E in Computer Science and
                   Engineering, achieving a total grade point average of 7.4%. My academic journey
@@ -290,14 +299,14 @@ function App() {
                   addressing real-world challenges.
                 </p>
               </div>
-              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px] " data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600">
                 <h3 className="absolute left-0 text-lg font-semibold">2024</h3>
                 <p>
                   Became more interested in IT and decided to try programming.
                   Underwent python course and made a project.
                 </p>
               </div>
-              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600">
                 <h3 className="absolute left-0 text-lg font-semibold">2024</h3>
                 <p>
                   Worked out in which field I want to build a developer's career
@@ -309,7 +318,7 @@ function App() {
         </section>
         <section className="py-8" id="contactme">
           <div className="container m-auto px-4">
-            <h2 className="text-2xl font-semibold" data-aos="fade-right">Contant me</h2>
+            <h2 className="text-2xl font-semibold" data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1200">Contant me</h2>
             <div className=" p-3 shadow-lg dark:bg-dark-2 sm:p-12 max-w-4xl m-auto">
               <form className="flex flex-col gap-5" onSubmit={onSubmit}>
                 <input
@@ -318,9 +327,7 @@ function App() {
                   required
                   placeholder="Your Name"
                   className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  data-aos="fade-right"
-                  data-aos-easing="linear"
-                  data-aos-duration="1000"
+                  data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600"
                 />
                 <input
                   type="text"
@@ -328,8 +335,7 @@ function App() {
                   required
                   placeholder="Your Email"
                   className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  data-aos="fade-left"
-                  data-aos-easing="linear" data-aos-duration="1200"
+                  data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600"
                 />
                 <input
                   type="text"
@@ -337,8 +343,7 @@ function App() {
                   required
                   placeholder="Your Phone"
                   className=" p-3 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  data-aos="fade-right"
-                  data-aos-easing="linear" data-aos-duration="1400"
+                  data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600"
                 />
                 <textarea
                   typeof="text"
@@ -347,15 +352,13 @@ function App() {
                   required
                   defaultValue=""
                   className=" p-3 h-52 bg-zinc-900 border-2 hover:scale-105 duration-300 rounded-2xl"
-                  data-aos="fade-left"
-                  data-aos-easing="linear" data-aos-duration="1600"
+                  data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600"
                 />
                 <div>
                   <button
                     type="submit"
                     className="w-full rounded-xl border-none bg-primary p-3 text-white transition hover:bg-blue-700 bg-cyan-500 hover:scale-105 duration-200"
-                    data-aos="fade-up"
-                    data-aos-easing="linear" data-aos-duration="1800"
+                    data-aos="fade-up" data-aos-easing="linear" data-aos-duration="600"
                   >
                     Send Message
                   </button>
@@ -373,12 +376,12 @@ function App() {
           </div>
           <div>
             <ul className="flex gap-4">
-              <li>
+              <li >
                 <a>
                   <img src={Facebook} className="w-5" />
                 </a>
               </li>
-              <li>
+              <li >
                 <a>
                   <img src={LinkedIn} className="w-5" />
                 </a>
@@ -389,7 +392,7 @@ function App() {
       </footer>
       {
         scrolling && (
-          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
+          <button className="fixed block right-8 bottom-0 w-24" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="800" onClick={() => {
             window.scrollTo(0, 0);
           }}>
             <img src={ArrowDown} />
